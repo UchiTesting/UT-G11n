bf1fdf70a1e31c76ee34c879b8c18196bbcb6a1a
UchiTesing <uchitesting57@gmail.com>
2023-07-24 17:57:29 +0200

    Enable G11n

---
+++ AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Localization;
+
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("ca280443-176a-4e68-ad45-d9e9d94da816")]
+
+[assembly: ResourceLocation("Resources")]
+[assembly: RootNamespace("UT_G11n")]
--- Program.cs
+++ Program.cs
@@ -1,7 +1,57 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Options;
+
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services
+    .AddLocalization(option => option.ResourcesPath = "Resources");
+
+builder.Services
+    //.AddControllersWithViews()
+    .AddMvc()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix
+    //, options =>
+    //{
+    //    options.ResourcesPath = "Resources";
+    //}
+    );
+
+
+builder.Services
+    .Configure<RequestLocalizationOptions>(options =>
+    {
+
+        var supportedCultures = new[]
+        {
+            "en",
+            "en-GB",
+            "en-US",
+            "fr",
+            "ja",
+            "pl",
+            "de"
+        };
+
+        // We can use methods to configure cultures
+        //options
+        //    .SetDefaultCulture(supportedCultures[0])
+        //    .AddSupportedCultures(supportedCultures)
+        //    .AddSupportedUICultures(supportedCultures);
+
+        var supportedCulturesList = new List<CultureInfo>();
+
+        System.Array.ForEach(supportedCultures, (cultureTag) => {
+            supportedCulturesList.Add(new CultureInfo(cultureTag));
+        });
+
+        // We can also use property counterparts
+        options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("pl");
+        options.SupportedCultures = supportedCulturesList;
+        options.SupportedUICultures = supportedCulturesList;
+    });
 
 var app = builder.Build();
 
@@ -17,6 +67,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+var options = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(options.Value);
+
 
 app.UseAuthorization();
 
